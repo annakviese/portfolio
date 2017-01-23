21fad169c314aacabf94e69bdd1a0635cbd78d37
Anna Kviese <anna.kviese@gmail.com>
2017-01-23 10:56:14 -0800

    added 2 other projects and flickity carousel

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.project-carousel').slick({
+   
+  });
+  $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+});
+});
+
